b92b588b676b3628f752d7544527d43734fe5af9
Lee Thomason <leethomason@gmail.com>
2017-08-11 16:02:06 -0700

    test files

---
+++ cent_rect.py
@@ -0,0 +1,35 @@
+from mecode import G
+
+SIZE_X = 20
+SIZE_Y = 10
+
+feedRate = 1000		# mm/min
+passDepth = 1.0		# mm
+spindleSpeed = 10000
+plungeRate = 300	# mm/min
+
+def initWood():
+	feedRate = 1000
+	passDepth = 0.5
+	spindleSpeed = 10000
+	plungeRate = 100
+
+g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None)
+initWood();
+
+g.write("(init)")
+g.relative()
+g.spindle("CW", spindleSpeed)
+g.move(y=-SIZE_Y/2)
+
+for d in range(0, 6):
+	g.write("(pass {})".format(d))
+	g.feed(plungeRate)
+	g.move(z=1.0)
+	g.feed(feedRate)
+	g.rect(x=SIZE_X, y=SIZE_Y)
+
+g.write("(teardown)")
+g.spindle()
+g.teardown()
+
---
+++ simple.py
@@ -0,0 +1,7 @@
+from mecode import G
+
+g = G()
+g.relative()
+#g.abs_arc(x=0, y=10)
+#g.set_home()
+#g.laser("dynamic")
