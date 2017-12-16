5d1aed683bc346375363eba562cfb375a6820c8c
Lee Thomason <leethomason@gmail.com>
2017-12-16 14:48:09 -0800

    new baffle code

--- baffle2.py
+++ baffle2.py
@@ -1,24 +1,31 @@
-from hole import hole, hole_abs
+from hole import hole_abs
 from mecode import G
 from material import *
-import math
 from utility import *
 
 inner_d = 12
 outer_d = 31.7
 cut_depth = -2
 tool_size = 3.175
-theta0 = 0 #math.radians(-10)
-theta1 = math.radians(90) #80
-theta2 = math.radians(180) #170
-theta3 = math.radians(270) #260
+half_tool = tool_size / 2
+theta0 = math.radians(-10)
+theta1 = math.radians(80)
+theta2 = math.radians(170)
+theta3 = math.radians(260)
 
-outer_r = outer_d / 2
-inner_r = inner_d / 2
-inset_r = outer_r - 5
+outer_r = outer_d / 2 + half_tool
+inner_r = inner_d / 2 - half_tool
+inset_r = outer_r - 5 + half_tool
 
 g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None)
 mat = initMaterial('aluminum')
+
+# rods that hold it together
+hole_abs(g, mat, cut_depth, tool_size, 3.5 / 2, -2, 12)
+hole_abs(g, mat, cut_depth, tool_size, 3.5 / 2, 2, -12)
+
+# channel for wires
+hole_abs(g, mat, cut_depth, tool_size, 5.6 / 2, -8, 8)
 
 MOVE_TO = 0
 ARC_CCW = 1
@@ -36,8 +43,8 @@ class CMD:
         if self.type == MOVE_TO:
             dx = self.x - prev.x
             dy = self.y - prev.y
-            length = math.sqrt(dx*dx + dy*dy)
-            return [(self.x - prev.x)/length, (self.y - prev.y)/length]
+            length = math.sqrt(dx * dx + dy * dy)
+            return [(self.x - prev.x) / length, (self.y - prev.y) / length]
         elif self.type == ARC_CCW:
             return [-math.sin(theta), math.cos(theta)]
         else:
@@ -52,6 +59,7 @@ class CMD:
         else:
             return [-math.sin(theta), math.cos(theta)]
 
+
 # first command must be a MOVE_TO
 path = [
     CMD(math.cos(theta0) * inner_r, math.sin(theta0) * inner_r, MOVE_TO),
@@ -63,6 +71,9 @@ path = [
     CMD(math.cos(theta3) * outer_r, math.sin(theta3) * outer_r, MOVE_TO),
     CMD(math.cos(theta0) * outer_r, math.sin(theta0) * outer_r, ARC_CCW)
 ]
+
+g.spindle('CW', mat['spindleSpeed'])
+g.feed(mat['feedRate'])
 
 g.absolute()
 g.spindle()
@@ -78,10 +89,10 @@ prev_y = 0
 
 for step in steps:
     z = z + step
-    for i in range(0, len(path)+1):
+    for i in range(0, len(path) + 1):
         current = i % len(path)
         prev = (i - 1 + len(path)) % len(path)
-        next = (i+1) % len(path)
+        n = (i + 1) % len(path)
 
         prev_x = x
         prev_y = y
@@ -101,11 +112,4 @@ for step in steps:
         elif cmd.type == ARC_CCW:
             g.arc2(x=x, y=y, i=-prev_x, j=-prev_y, direction='CCW')
         else:
-            g.arc2(x=x, y=y, i=-prev_x, j=-prev_y, direction= 'CW')
-
-# rods that hold it together
-#hole_abs(g, mat, cut_depth, tool_size, 3.5 / 2, -2, 12)
-#hole_abs(g, mat, cut_depth, tool_size, 3.5 / 2, 2, -12)
-
-# channel for wires
-#hole_abs(g, mat, cut_depth, tool_size, 5.6 / 2, -8, 8)
+            g.arc2(x=x, y=y, i=-prev_x, j=-prev_y, direction='CW')
