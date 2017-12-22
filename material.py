062f2066a3771979838d55a40b0c185d3cf8a6d9
Lee Thomason <leethomason@gmail.com>
2017-12-21 16:19:48 -0800

    comment clean up

--- material.py
+++ material.py
@@ -7,39 +7,38 @@ import sys
 # wood-2.0
 
 materials = [
-    # 1/8, 3mm
+    # -------------------------- #
     {"name": "wood",
      "tool_size": 3.125,
      "feed_rate": 1000,
      "pass_depth": 0.5,
-     "spindle_speed": 10000,
+     "spindle_speed": 1000,
      "plunge_rate": 100},
 
-    # 3.125
+    # -------------------------- #
     {"name": "aluminum",
      "tool_size": 3.125,
      "feed_rate": 150,
      "pass_depth": 0.05,  # woh. slow and easy for aluminum
-     "spindle_speed": 10000,
+     "spindle_speed": 1000,
      "plunge_rate": 12},
 
-    # 1.0mm pcb
+    # -------------------------- #
     {"name": "fr",
      "tool_size": 1.0,
      "feed_rate": 200,  # was: 280 felt a little fast?
      "pass_depth": 0.15,
-     "spindle_speed": 10000,
+     "spindle_speed": 1000,
      "plunge_rate": 30},
 
-    # 0.8mm pcb
     {"name": "fr",
      "tool_size": 0.8,
      "feed_rate": 160,
      "pass_depth": 0.15,
-     "spindle_speed": 10000,
+     "spindle_speed": 1000,
      "plunge_rate": 30},
 
-    # testing
+    # ------testing------------ #
     {"name": "air",
      "tool_size": 3.125,
      "feed_rate": 1200,
@@ -91,35 +90,6 @@ def initMaterial(name: str):
                 return m
 
     raise RuntimeError('material "{}" not defined'.format(name))
-
-
-'''
-For PCB
-
-1.6mm
-Feed rate: 14.173 in/min (360 mm/min)
-Plunge rate: 1.81 in/min (30 mm/min)
-Spindle speed: 12,000 RPM
-Max pass depth: 0.005" (0.13 mm)
-
-Aggressive:
-Feed rate: 59 in/min (1500 mm/min)
-Plunge rate: 15 in/min (381 mm/min)
-Spindle speed: 16,400 RPM
-Max pass depth: 0.006" (0.15 mm)
-
-0.8mm
-Feed rate: 14.173 in/min (360 mm/min)
-Plunge rate: 1.81 in/min (30 mm/min)
-Spindle speed: 12,000 RPM
-Max pass depth: 0.006" (0.15 mm)
-
-Aggressive:
-Feed rate: 59 in/min (1500 mm/min)
-Plunge rate: 15 in/min (381 mm/min)
-Spindle speed: 16,400 RPM
-Max pass depth: 0.006" (0.15 mm)
-'''
 
 
 def main():
