b0e1dee327343a2aaa9ecabbe2888dfd5fc2754b
Lee Thomason <leethomason@gmail.com>
2018-02-13 20:36:05 -0800

    improve the drill script

--- drill.py
+++ drill.py
@@ -6,17 +6,15 @@
 # done as one pass. (Tool change wolud be straightforward, I
 # just don't a machine that can do it reliably.)
 #
-
-import math
-import sys
 from mecode import G
 from material import *
 from utility import *
 
-def drill(g, mat, cutDepth, points):
-    nPlunge = 1 + int(-cutDepth / (0.05 * mat['plunge_rate']))
-    if cutDepth >= 0:
-        raise RunTimeError('Cut depth must be less than zero.')
+
+def drill(g, mat, cut_depth, points):
+    nPlunge = 1 + int(-cut_depth / (0.05 * mat['plunge_rate']))
+    if cut_depth >= 0:
+        raise RuntimeError('Cut depth must be less than zero.')
 
     if g is None:
         g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None)
@@ -24,13 +22,13 @@ def drill(g, mat, cutDepth, points):
     g.comment("Drill")
     g.comment("init ABSOLUTE:")
     g.comment("  material: " + mat['name'])
-    g.comment("  depth: " + str(cutDepth))
+    g.comment("  depth: " + str(cut_depth))
     g.comment("  num pecks: " + str(nPlunge))
 
     g.absolute()
     g.feed(mat['feed_rate'])
 
-    sort_shortest_path(points);
+    sort_shortest_path(points)
     g.comment("  num points: " + str(len(points)))
 
     g.move(z=CNC_TRAVEL_Z)
@@ -46,14 +44,14 @@ def drill(g, mat, cutDepth, points):
 
         # move up and down in stages.
         # don't move up on the last step, and hold at the bottom of the hole.
-        zStep = cutDepth / nPlunge;
-        for i in range(0, nPlunge-1):
-            g.move(z=zStep * (i+1))
+        zStep = cut_depth / nPlunge
+        for i in range(0, nPlunge - 1):
+            g.move(z=zStep * (i + 1))
             g.dwell(0.250)
             g.move(z=zStep * i)
             g.dwell(0.250)
 
-        g.move(z=cutDepth)
+        g.move(z=cut_depth)
         g.dwell(0.250)
 
         g.move(z=0)
@@ -66,12 +64,13 @@ def drill(g, mat, cutDepth, points):
     # Leaves the head at (0, 0, CNC_TRAVEL_Z)
     g.move(x=0, y=0)
 
+
 def main():
     try:
         float(sys.argv[3])
         isNumberPairs = True
     except:
-        isNumberPairs = (sys.argv[3].find(',') >= 0)
+        isNumberPairs = len(sys.argv) > 3 and (sys.argv[3].find(',') >= 0)
 
     if len(sys.argv) < 4:
         print('Drill a set of holes.')
@@ -80,14 +79,13 @@ def main():
         print('  drill material depth x0,y0 x1,y1 (etc)')
         print('  drill material depth x0 y0 x1 y1 (etc)')
         print('Notes:')
-        print('  Runs in ABSOLUTE coordinates.')
-        print('  Assumes bit CENTERED at 0,0')
-        print('  millimeters, travel Z={}'.format(CNC_TRAVEL_Z))
+        print('  Runs in absolute coordinates.')
+        print('  Travel Z={}'.format(CNC_TRAVEL_Z))
         sys.exit(1)
 
-    param    = initMaterial(sys.argv[1])
+    param = initMaterial(sys.argv[1])
     cutDepth = float(sys.argv[2])
-    points  = []
+    points = []
 
     if not isNumberPairs:
         filename = sys.argv[3]
@@ -98,13 +96,14 @@ def main():
             for i in range(3, len(sys.argv)):
                 comma = sys.argv[i].find(',')
                 x = float(sys.argv[i][0:comma])
-                y = float(sys.argv[i][comma+1:])
-                points.append({'x':x, 'y':y})
+                y = float(sys.argv[i][comma + 1:])
+                points.append({'x': x, 'y': y})
         else:
             for i in range(3, len(sys.argv), 2):
-                points.append({'x':float(sys.argv[i+0]), 'y':float(sys.argv[i+1])})
+                points.append({'x': float(sys.argv[i + 0]), 'y': float(sys.argv[i + 1])})
 
     drill(None, param, cutDepth, points)
+
 
 if __name__ == "__main__":
     main()
