dc3e1c031ee7cdaa09352934d8dfab3c9fc18ce0
Lee Thomason <leethomason@gmail.com>
2018-04-01 19:15:51 -0700

    dimension tweaks

--- baffle3.py
+++ baffle3.py
@@ -18,13 +18,13 @@ outer_r = outer_d / 2
 inner_r = inner_d / 2
 inset_r = outer_r - 5
 
-rod_d = 3.6         #
-rod_x = 0           #
-rod_y = 11          #
+rod_d = 3.6  #
+rod_x = 0  #
+rod_y = 11  #
 
-channel_d = 5.8     #
-channel_x = -8      #
-channel_y = 8       #
+channel_d = 5.8  #
+channel_x = -8  #
+channel_y = 8  #
 
 g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None)
 g.comment("Material: " + mat['name'])
@@ -52,14 +52,16 @@ def g_arc(g, theta, r, direction, z=None):
         g.arc2(x=x, y=y, i=i, j=j, direction=direction)
     pass
 
+
 def g_move(g, theta, r, z=None):
     if z is None:
         g.abs_move(x=x_r(theta, r), y=y_r(theta, r))
     else:
         g.abs_move(x=x_r(theta, r), y=y_r(theta, r), z=z)
 
+
 def path(g, z, dz):
-    g_move(g, theta0, inner_r, z + dz/2)
+    g_move(g, theta0, inner_r, z + dz / 2)
     g_arc(g, theta1, inner_r, 'CW')
     g_move(g, theta1, outer_r, z + dz)
     g_arc(g, theta2, outer_r, 'CCW')
