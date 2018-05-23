95a832054f259766f0c8630c4020172a5187f1c4
Lee Thomason <leethomason@gmail.com>
2018-05-22 23:27:04 -0700

    cleanup and formatting

--- README.md
+++ README.md
@@ -3,12 +3,11 @@ A collection of CNC code for saber construction.
 
 # NanoPCB
 Nano PCB takes an text-art PCB and creates gcode for sending to a CNC.
-Basically markdown from printed circuit board cutting.
+Basically markdown for printed circuit board cutting.
 
 ## Goals:
 1. Simple: from a text file to gcode in one step
-2. Basic CNC machine. The CNC machine I wrote it for is basic and inaccurate.
-   The NanoPCB is tailored to a low end machine:
+2. Basic CNC machine.
    - Isolate circuit, drill, and cut PCB without a bit change. (I use 1.0mm
      bits, although 0.8mm might be better.)
    - Account for significant runout. NanoPCB creates straight cuts that are
@@ -16,86 +15,35 @@ Basically markdown from printed circuit board cutting.
 
 Currently it only works single-sided. It would be straightforward to support
 the flip, the code just needs a way to define the two sides and the flipping
-axis.
+axis. It can print the circuit flipped if you want to solder on the down side.
 
-## Info mode
-There are several command line options. The most useful - before you generate
-gcode - is to use -i (--info) to print information about the circuit.
+## Legend
 
-## Examples:
+* ````#````         Starts a comment line.
+* ````[ ]````       Square brackets define a cutting border.
+* ````%````         A percent defines a cutting path. The path must close, but can curve around for printing oddly shaped boards.
+* ````a-z````       Letters define drill holes.
+* ````# +M 2.2````  Defines a mounting hole, drill hole, etc. (Not isolated.)
 
-This drills 3 holes, isolates them, and will be cut square to fit.
-````
-o--o
-|  |
-o--+
-
-# The symbols -, |, and + draw traces.
-# Most other things are interpreted as drill holes.
-````
-- nDrill points = 3
-- rows/cols = 6,5
-- size (on tool center) = 6.35,5.08
-
-
-This drills 6 holes, isolates them, and will be cut square to fit.
-````
-1 2 3
-| | |
-o o o
-
-# 1, 2, 3 are all drill holes, but allow you to comment
-# 1 ground
-# 2 vcc
-# 3 clock
-````
-- nDrill points = 6
-- rows/cols = 7,5
-- size (on tool center) = 7.62,5.08
-
-
-This drills 6 holes, isolates them, and will be cut to larger bounds.
-````
-[           ]
-    g v c
-    | | |
-    o o o
-[           ]
-
-# The [ and ] characters make the cutting bounds larger. It will be
-# cut so that the brackets are NOT removed. (So they expand the bounds
-# just like drill holes do...except without the drill holes.)
-````
-- nDrill points = 6
-- rows/cols = 15,7
-- size (on tool center) = 17.78,7.62
-
-
-If you want to specify a cutting path, you may do so. Use the percent
-character; just be sure to define a full path. NanoPCB follows the
-percent character to trace out a path.
+## Example
 
 ````
-%%   %%%%%%   %%
-%              %
-  oo        oo
+#################################
+# +M 2.2  Mounting
+# # # # # # # # # # # # # # # # #
+[               ]
+  M           M 
 
-  oo        oo
+V o     o-r g   b
+| |         |   |
+V o     o---o   o
 
-% oo        oo %
-%              %
-%%%%%      %%%%%
-    %      %
+G   o o   o   o
+|
+G---o-o---o---o
+|
+G o-o o-o o-o o-o 
 
-      o--o
-    %      %
-    %%    %%
+  M           M 
+[               ]
 ````
-- nDrill points = 14
-- rows/cols = 18,16
-- size (on tool center) = 19.05,16.51
-
-
-
-
-
--- nanopcb.py
+++ nanopcb.py
@@ -1,10 +1,12 @@
 # turn ascii art into a pcb. for real.
 
 from mecode import G
-from material import *
+from material import initMaterial
 from utility import *
 from drill import drill_points
 from hole import hole_or_drill
+import argparse
+import sys
 
 SCALE = 2.54 / 2
 NOT_INIT = 0
@@ -220,7 +222,8 @@ def print_to_console(pcb, mat, n_cols, n_rows, drill_ascii, cut_path, cut_size, 
                      sign(cut_path[n].y - cut_path[i].y))
         p = Point(cut_path[i].x, cut_path[i].y)
         while True:
-            output_rows[int(p.y)] = output_rows[int(p.y)][0:int(p.x)] + '%' + output_rows[int(p.y)][int(p.x + 1):]
+            output_rows[int(p.y)] = output_rows[int(p.y)][0:int(
+                p.x)] + '%' + output_rows[int(p.y)][int(p.x + 1):]
             if p == cut_path[n]:
                 break
             p.x += step.x
@@ -232,14 +235,16 @@ def print_to_console(pcb, mat, n_cols, n_rows, drill_ascii, cut_path, cut_size, 
     for h in holes:
         diameter = h["diameter"]
         cut_type = hole_or_drill(None, mat, -1.0, diameter / 2)
-        print("Hole ({}): d = {}  pos = {}, {}".format(cut_type, diameter, h["x"], h["y"]))
+        print("Hole ({}): d = {}  pos = {}, {}".format(
+            cut_type, diameter, h["x"], h["y"]))
 
-    print('Number of drill ={}'.format(len(drill_ascii)))
-    print('rows/cols = {},{}'.format(n_cols, n_rows))
+    print('Number of drill holes = {}'.format(len(drill_ascii)))
+    print('Rows/Cols = {} x {}'.format(n_cols, n_rows))
 
     sx = cut_size.x - mat['tool_size']
     sy = cut_size.y - mat['tool_size']
-    print('size (after cut) = {} x {} mm ({:.2f} x {:.2f} in)'.format(sx, sy, sx/25.4, sy/25.4))
+    print('Size (after cut) = {} x {} mm ({:.2f} x {:.2f} in)'.format(
+        sx, sy, sx/25.4, sy/25.4))
 
 
 def rc_to_xy_normal(x, y, n_cols, n_rows):
@@ -252,7 +257,7 @@ def rc_to_xy_flip(x, y, n_cols, n_rows):
 
 def nanopcb(filename, mat, pcb_depth, drill_depth,
             do_cutting, info_mode, do_drilling, flip):
-            
+
     if pcb_depth > 0:
         raise RuntimeError("cut depth must be less than zero.")
     if drill_depth > 0:
@@ -322,12 +327,15 @@ def nanopcb(filename, mat, pcb_depth, drill_depth,
 
     # Create a cut_path, if not specified, to simplify the code from here on:
     if cut_path is None:
-        cut_path = [Point(0, 0), Point(n_cols - 1, 0), Point(n_cols - 1, n_rows - 1), Point(0, n_rows - 1)]
+        cut_path = [Point(0, 0), Point(n_cols - 1, 0),
+                    Point(n_cols - 1, n_rows - 1), Point(0, n_rows - 1)]
 
     cut_min_dim, cut_max_dim = bounds_of_points(cut_path)
-    cut_size = Point((cut_max_dim.x - cut_min_dim.x) * SCALE, (cut_max_dim.y - cut_min_dim.y) * SCALE)
+    cut_size = Point((cut_max_dim.x - cut_min_dim.x) * SCALE,
+                     (cut_max_dim.y - cut_min_dim.y) * SCALE)
 
-    print_to_console(pcb, mat, n_cols, n_rows, drill_ascii, cut_path, cut_size, holes)
+    print_to_console(pcb, mat, n_cols, n_rows, drill_ascii,
+                     cut_path, cut_size, holes)
     if info_mode is True:
         sys.exit(0)
 
@@ -361,7 +369,8 @@ def nanopcb(filename, mat, pcb_depth, drill_depth,
             c = PtPair(p0.x, p0.y, p1.x, p1.y)
             isolation_pairs.append(c)
 
-    g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None, print_lines=False)
+    g = G(outfile='path.nc', aerotech_include=False,
+          header=None, footer=None, print_lines=False)
 
     g.comment("NanoPCB")
 
@@ -393,7 +402,7 @@ def nanopcb(filename, mat, pcb_depth, drill_depth,
     g.move(z=CNC_TRAVEL_Z)
 
     if do_drilling:
-        drill_pts = [];
+        drill_pts = []
         for da in drill_ascii:
             drill_pts.append(rc_to_xy(da['x'], da['y'], n_cols, n_rows))
 
