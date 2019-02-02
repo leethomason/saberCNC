33c8b6971a5d0033277a9da9ee98588c8e792afd
Lee Thomason <leethomason@gmail.com>
2019-02-02 11:33:34 -0800

    fixes to plane, hill code

--- covertec.py
+++ covertec.py
@@ -3,6 +3,84 @@ from material import init_material
 from utility import CNC_TRAVEL_Z, GContext
 import argparse
 import math
+from plane import plane, flat
+from hole import hole
+
+def xy_circle(x, r):
+    xy = math.sqrt(r * r - x * x)
+    return xy
+
+def z_tool(dx, r_ball, r_hill):
+    z = math.sqrt(math.pow(r_ball + r_hill, 2) + dx * dx)
+    return r_ball + r_hill - z
+
+def hill(g, mat, diameter, dx, dy, ball_cutter):
+    r_hill = diameter / 2
+    pht = mat['tool_size'] * 0.8
+    ht = mat['tool_size'] * 0.5
+    hy = dy / 2
+    doc = mat['pass_depth']
+
+    def rough(bias):
+        g.move(y=hy * bias)
+        d = 0
+
+        while(True):
+            x = math.sqrt(r_hill*r_hill - math.pow(r_hill - (doc - d), 2.0))
+            cut_y = hy - x - pht
+
+            if cut_y < 0:
+                g.move(z=-d)
+                g.move(y=-hy * bias)
+                return
+
+            d = d - doc
+            g.move(x=dx, z=-doc/2)
+            g.move(x=-dx, z=-doc/2)
+            flat(g, mat, dx, -cut_y * bias)
+
+    def smooth(bias):
+        base_step = 0.4
+        y = 0
+        d = 0
+
+        while y != hy:
+            g.move(x=dx)
+            g.move(x=-dx)
+
+            step = (1 - y / r_hill) * base_step
+            if y + step > hy:
+                step = hy - y
+                y = hy
+            else:
+                y += step
+
+            g.move(y=step * bias)
+
+            zt = z_tool(y, ht, r_hill)
+            g.move(z = zt - d)
+            d = zt
+        
+        g.move(z=-d) 
+        g.move(y=-y * bias)
+
+    with GContext(g):
+        g.comment('hill')
+        g.relative()
+        g.feed(mat['feed_rate'])
+        g.spindle('CW', mat['spindle_speed'])
+
+        rough(1)
+        rough(-1)
+
+        smooth(1)
+        smooth(-1)
+
+        g.move(z=CNC_TRAVEL_Z)
+        g.spindle()
+
+        #g.move(z=CNC_TRAVEL_Z)
+        #hole(g, mat, -2.0, 4.0)
 
 def covertec(g, mat, diameter, width, dy, overlap):
 
@@ -80,7 +158,8 @@ def main():
     args = parser.parse_args()
     g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None, print_lines=False)
     mat = init_material(args.material)
-    covertec(g, mat, args.diameter, args.dy, args.dy, args.overlap)
+    # covertec(g, mat, args.diameter, args.dy, args.dy, args.overlap)
+    hill(g, mat, args.diameter, args.dx, args.dy, True)
 
 
 if __name__ == "__main__":
--- plane.py
+++ plane.py
@@ -5,54 +5,58 @@ from material import init_material
 from utility import *
 import argparse
 
+def flat(g, mat, dx, dy, overlap_fraction=0.8):
+    with GContext(g):
+        g.relative()
 
-def plane(g, mat, depth, dx, dy, overlap=0.7):
-    if overlap <= 0.1 or overlap >= 1:
+        num_lines = int(math.ceil(abs(dy) / (mat['tool_size'] * overlap_fraction))) + 1
+        
+        line_step = 0
+        if num_lines > 1:
+            line_step = dy / (num_lines - 1)
+
+        g.comment("Flat")
+        for i in range(0, num_lines):
+            g.move(x=dx)
+            g.move(x=-dx)
+            if i < num_lines - 1:
+                g.move(y=line_step)
+
+        g.move(y=-dy)
+        g.comment("...flat done")
+
+def plane(g, mat, depth, dx, dy, overlap_fraction=0.8):
+    if overlap_fraction <= 0.1 or overlap_fraction >= 1:
         raise RuntimeError("step must be between 0 and 1 exclusive")
-
+ 
     with GContext(g):
         g.comment("Plane depth = {} size = {}, {}".format(depth, dx, dy))
         g.relative()
 
-        # a lap is a cut out and back
-        num_lap = int(math.ceil(dy / (mat['tool_size'] * overlap * 2.0)))
-        if num_lap > 1:
-            lap_step = dy / (num_lap - 0.5)
-        else:
-            lap_step = 0
-        num_down = int(math.ceil(-depth / mat['pass_depth']))
-        down_step = depth / num_down
-
         g.spindle('CW', mat['spindle_speed'])
         g.feed(mat['feed_rate'])
 
-        for j in range(0, num_down):
-            for i in range(0, num_lap):
-                if i == 0:
-                    g.move(x=dx, z=down_step/2)
-                    g.move(x=-dx, z=down_step/2)
+        def path(g, dz):
+            # first line to distribute depth cut.
+            g.move(x=dx, z=dz/2)
+            g.move(x=-dx, z=dz/2)
 
-                g.move(x=dx)
-                g.move(y=lap_step / 2)
-                g.move(x=-dx)
-                # don't go out of the plane
-                if i < num_lap - 1:
-                    g.move(y=lap_step/2)
-            g.move(y=-(num_lap-0.5) * lap_step)
+            # now the business of cutting.
+            flat(g, mat, dx, dy, overlap_fraction)
 
-        # and back up to where we started.
+        run_3_stages(path, g, calc_steps(depth, -mat['pass_depth']))
         g.move(z=-depth)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Cut a plane. Useful for bed leveling. Should start at origin (0,0,0). ' +
+        description='Cut a plane. Origin of the plane is the current location. ' +
                     'Does not account for tool thickness, so the dx, dy will be fully cut and ' +
-                    'on the tool center.')
+                    'on the tool center. dx/dy can be positive or negative.')
     parser.add_argument('material', help='The material to cut in standard machine-material-size format.', type=str)
     parser.add_argument('depth', help='Depth of the cut. Must be negative.', type=float)
-    parser.add_argument('dx', help="Width of the plane.", type=float)
-    parser.add_argument('dy', help="Height of the plane.", type=float)
+    parser.add_argument('dx', help="Size in x.", type=float)
+    parser.add_argument('dy', help="Size in y.", type=float)
     args = parser.parse_args()
 
     mat = init_material(args.material)
